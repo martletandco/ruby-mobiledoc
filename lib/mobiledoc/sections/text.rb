617b42cd9cf2e46b701e420d861da21b61d40237
Joshua Vermeulen <joshua@martletandco.co.nz>
2021-05-20 14:00:47 +1000

    Update spec to cover overlap cases

--- lib/mobiledoc/sections/text.rb
+++ lib/mobiledoc/sections/text.rb
@@ -9,7 +9,22 @@ module Mobiledoc::Section
       @tag = options[:tag] || :p
     end
 
+    # Append adds text with optional markups to the end of the section. The
+    # order of the markups has an impact of where the final marker breaks
+    # will be. Once this supports edits, the application on markups there will
+    # also have an impact on marker placement
     def append(text, markups = [])
+      we want to hold a list of ranges here, so we can support edits later, but
+      also because it makes serialisation easier too
+
+      Might need to find and join ranges which match markups. Need to decide 
+      what the semanics are here re appending. Might be different to applying
+      to a range. Perhaps we should rejig the order to better line up with existing markups?
+
+      Is appending: adding text then applying markups to that range? A shorthand
+
+      OR perhaps this API is different from an 'editor' as it is programatic, rather than a UI?
+      So there _could_ be a "natural" order to the way markers are built
       segments << [text, markups] unless text.nil?
     end
 
@@ -37,6 +52,7 @@ module Mobiledoc::Section
 
         text = current_segment.first
         current_markups = current_segment.last.uniq
+        # @incomplete: I think we need to either run this loop backwards or run two passes (maybe a longest chain sorting)
         next_markups = next_segment&.last&.uniq || []
 
         # Ensure we only open markups that aren't already open
@@ -44,9 +60,13 @@ module Mobiledoc::Section
 
         markups_open_in_current_segment = open_markups + markups_to_open
         # Any markups that are already open, or have just been opened, that aren't in the next segment
+        # [underline, strikethrough]
+        # [strikethrough]
         markups_to_close = markups_open_in_current_segment - next_markups
 
-        markup_indexes_to_open = markups_to_open.map { |markup| ordered_markups.index(markup) }
+        markup_indexes_to_open = markups_to_open
+          .sort_by { |markup| [next_markups.include?(markup) ? 1 : 0, markup.tag_name] }
+          .map { |markup| ordered_markups.index(markup) }
         number_markups_closed = markups_to_close.length 
 
         # @magic: '0' is a text markup. Also it might be clearer if this was calling a
